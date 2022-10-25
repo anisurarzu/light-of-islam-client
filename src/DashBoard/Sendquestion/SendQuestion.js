9ed3f92c2f91ef84d8a9d39a025e1d732de90ad3
anisurarzu <anisur35-2163@diu.edu.bd>
2022-10-25 23:55:45 +0600

    qsn sending design fix

--- src/DashBoard/Sendquestion/SendQuestion.js
+++ src/DashBoard/Sendquestion/SendQuestion.js
@@ -35,80 +35,89 @@ const SendQuestion = () => {
         <div />
       )} */}
 
-
-      {/* <form
-        className="flex flex-col text-left  p-4 mt-0 xl:mx-44"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <label className="pb-2" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-          {...register("name")}
-          defaultValue={user.displayName}
-          placeholder="enter your name"
-          required
-        />
-        <label className="pb-2" htmlFor="name">
-          Email
-        </label>
-        <input
-          className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-          defaultValue={user.email}
-          {...register("email")}
-          required
-        />
-        <label className="py-2" htmlFor="name">
-          Write your question here
-        </label>
-        <textarea
-          className="w-full focus:ring-blue-400 px-2 py-2 text-gray-700 bg-gray-200 rounded"
-          {...register("question")}
-          required
-          placeholder="enter your text here"
-          rows="6"
-        />
-        <label className="py-2" htmlFor="name">
-          Gender
-        </label>
-        <select
-          className="w-full px-2 py-2 text-gray-700 bg-gray-200 rounded"
-          {...register("gender")}
-          required
+      {
+        <form
+          className="w-full max-w-lg flex flex-col justify-center text-center  ml-auto mr-auto"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          <option value="male">male</option>
-          <option value="female">female</option>
-          <option value="other">other</option>
-        </select>
-        <input
-          className="py-2 rounded mt-4 service-btn text-white"
-          type="submit"
-        />
-      </form> */}
-      <div className="text-white md:shadow md:shadow-md rounded-md question-from">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 text-left ml-1 "
+            for="grid-first-name"
+          >
+            Name
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            {...register("name")}
+            defaultValue={user.displayName}
+            placeholder="enter your name"
+            required
+          />
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 text-left ml-1 "
+            htmlFor="name"
+          >
+            Email
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            defaultValue={user.email}
+            {...register("email")}
+            required
+          />
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 text-left ml-1 "
+            htmlFor="name"
+          >
+            Write your question here
+          </label>
+          <textarea
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            {...register("question")}
+            required
+            placeholder="enter your text here"
+            rows="6"
+          />
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 text-left ml-1 "
+            htmlFor="name"
+          >
+            Gender
+          </label>
+          <select
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            {...register("gender")}
+            required
+          >
+            <option value="male">male</option>
+            <option value="female">female</option>
+            <option value="other">other</option>
+          </select>
+          <input
+            className="py-2 rounded mt-4 service-btn text-white"
+            type="submit"
+          />
+        </form>
+      }
+      {/*  <div className="text-white md:shadow md:shadow-md rounded-md question-from">
         <br />
         <br />
-      <h5 className="text-xl">Send Your Question With Valid Information!</h5>
-      <br /><br />
-      {message ? (
-        <div className="mx-8">
-          <Alert message={message} type="success" />
-        </div>
-      ) : (
-        <div />
-      )}
+        <h5 className="text-xl">Send Your Question With Valid Information!</h5>
+        <br />
+        <br />
+        {message ? (
+          <div className="mx-8">
+            <Alert message={message} type="success" />
+          </div>
+        ) : (
+          <div />
+        )}
         <form className="w-full max-w-lg flex flex-col justify-center text-center  ml-auto mr-auto">
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-              <label
-                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 text-left ml-1 "
-                for="grid-first-name"
-              >
-                First Name
-              </label>
+              
               <input
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                
                 id="grid-first-name"
                 type="text"
                 placeholder="Jane"
@@ -170,7 +179,8 @@ const SendQuestion = () => {
               >
                 Message
               </label>
-              <input
+              <textarea
+                {...register("question")}
                 className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-5 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="text"
                 type="text"
@@ -178,13 +188,13 @@ const SendQuestion = () => {
               />
             </div>
           </div>
-          <button className=" text-sm btn-donate text-white font-bold text-xl rounded-full  px-5 p-2 mt-2 ">
+          <input type className=" text-sm btn-donate text-white font-bold text-xl rounded-full  px-5 p-2 mt-2 ">
             Send
-          </button>
+          </input>
           <br />
           <br />
         </form>
-      </div>
+      </div> */}
     </div>
   );
 };
